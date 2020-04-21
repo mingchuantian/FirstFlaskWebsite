72d327475c1bc99770461b720de8c877eb5b3f5c
ming <1120509786@qq.com>
2020-04-21 10:30:45 -0700

    update on flask website

--- hello.py
+++ hello.py
@@ -1,6 +1,28 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
 
-@app.route('/')
+
+#bootstrap initialization
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+#wtf-form config
+app.config['SECRET_KEY'] = '960812'
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?')
+    submit = SubmitField('Submit')       
+
+
+
+@app.route('/', methods=['GET','POST']) #register the view function as a handler for GET and POST requests. 
 def index():
-    return render_template('index.html')
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', myform=form, name=name)
+
